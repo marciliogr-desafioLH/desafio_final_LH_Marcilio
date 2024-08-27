dd73fe36f69e198e9442ad9b51abed8461545fca
Marcilio de Godoy Rodrigues <marciliogr.desafiolh@gmail.com>
2024-08-27 01:43:32 +0000

    hotfix_stg_sales_reason

--- models/staging/stg_sales_reason.sql
+++ models/staging/stg_sales_reason.sql
@@ -2,7 +2,7 @@ with
     raw_data as (
         select
             cast(salesreasonid as int) as pk_sales_reason_id
-            , cast(name as varchar) as fk_territory_id
+            , cast(name as varchar) as sales_reason_name
             , cast(reasontype as varchar) as sales_quota
             --unused columns:
             --modifieddate 
